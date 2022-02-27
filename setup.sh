4e7649912fe87f4b6148fbf2a0c256b27ad746db
Nick Henderson <nhenderson019@gmail.com>
2022-02-27 11:06:00 +1000

    Add iTerm config and auto install fonts

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/zsh
 
+echo "Installing required Nerd Fonts"
+brew tap homebrew/cask-fonts
+brew install font-fira-code --cask
+
+echo "Don't forget to set 'fira' as the iTerm font for the Main profile'
+
+echp "Installing Starship"
 brew install starship
 
 echo "Creating ZSH link"
 rm ~/.zshrc
 ln zshrc ~/.zshrc
 zsh
+
+echo "Creating VimRC link"
+rm ~/.vimrc
+ln vimrc ~/.vimrc
 
