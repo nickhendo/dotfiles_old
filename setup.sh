e4fc0b4a4399ef9f9038ad2141412dd1b4f94663
Nick Henderson <nhenderson019@gmail.com>
2022-02-27 11:52:56 +1000

    Changing some more settings

--- setup.sh
+++ setup.sh
@@ -6,8 +6,18 @@ brew install font-fira-code --cask
 
 echo "Don't forget to set 'fira' as the iTerm font for the Main profile'
 
-echp "Installing Starship"
+echo "Installing Starship"
 brew install starship
+
+echo "Changing git red color to one better suited for dark backgrounds"
+git config --global color.branch.remote "red bold"
+git config --global color.status.untracked "red bold"
+git config --global color.status.changed "red bold"
+
+echo "Creating Starship config link"
+mkdir -p ~/.config
+rm ~/.config/starship.toml
+ln starship.toml ~/.config/starship.toml
 
 echo "Creating ZSH link"
 rm ~/.zshrc
